5f36efd41798739ff600222e564613440eb11609
ssandir <sandi.rezonja@gmail.com>
2019-12-02 22:00:47 +0100

    set ip

--- provider-api/src/main/java/com/kumuluzee/blocker/provider/api/ProviderApi.java
+++ provider-api/src/main/java/com/kumuluzee/blocker/provider/api/ProviderApi.java
@@ -10,6 +10,24 @@ public class ProviderApi {
         return "Hellow world! <br> I provide.";
     }
 
+    static String ipProvider = "00";
+    static String  ipAi = "11";
+
+    @GET
+    @Path("/providerip/{newIpProvider}")
+    public String setIpProvider(@PathParam("newIpProvider") String newIpProvider) {
+        ipProvider = newIpProvider;
+        return "New provider ip is: " + ipProvider;
+    }
+
+    @GET
+    @Path("/aiip/{newIpAi}")
+    public String setAi(@PathParam("newIpAi") String newIpAi) {
+        ipAi = newIpAi;
+        return "New ai ip is: " + ipAi;
+    }
+
+
     @GET
     @Path("/info")
     @Produces(MediaType.APPLICATION_JSON)
@@ -18,7 +36,7 @@ public class ProviderApi {
         String r = new StringBuilder()
                 .append("\"clani\": [\"sr3182\"],")
                 .append("\"opis_projekta\": \"Projekt implementira naprednega blockerja spletnih vsebin (najprej spoilerjev).\",")
-                .append("\"mikrostoritve\": [\"TBDhttp://35.189.96.118:8081/v1/orders\", \"TBDhttp://35.197.209.159:8080/v1/customers/\"],")
+                .append("\"mikrostoritve\": [\"http://" + ipProvider + ":8080/provider\", \"http://" + ipAi + ":8080/ai\"],")
                 .append("\"github\": [\"https://github.com/Content-blocker/content-provider\", \"https://github.com/Content-blocker/ai\"],")
                 .append("\"travis\": [\"https://travis-ci.org/Content-blocker/content-provider\", \"https://travis-ci.org/Content-blocker/ai\"],")
                 .append("\"dockerhub\": [\"https://cloud.docker.com/repository/docker/sr123/provider/\", \"https://cloud.docker.com/repository/docker/sr123/ai/\"]")
