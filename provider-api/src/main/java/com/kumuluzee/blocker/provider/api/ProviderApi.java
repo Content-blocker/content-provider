b0d0a6ccf2a67d5c813dc6268cc6ecfcf67ddcb6
ssandir <sandi.rezonja@gmail.com>
2019-12-02 21:22:14 +0100

    Changed api

--- provider-api/src/main/java/com/kumuluzee/blocker/provider/api/ProviderApi.java
+++ provider-api/src/main/java/com/kumuluzee/blocker/provider/api/ProviderApi.java
@@ -2,17 +2,24 @@ package com.kumuluzee.blocker.provider.api;
 
 import javax.ws.rs.*;
 
-@Path("/call")
+@Path("/provider")
 public class ProviderApi {
     @GET
-    @Path("/here")
-    public String hello() {
-        return "hello i'm here";
+    public String getResources() {
+        return "Hellow world! <br> I provide.";
     }
 
     @GET
-    @Path("stuff")
-    public String getResources() {
-        return "string123";
+    @Path("/info")
+    public String info() {
+        String r = new StringBuilder()
+                .append("\"clani\": [\"sr3182\"],")
+                .append("\"opis_projekta\": \"Projekt implementira naprednega blockerja spletnih vsebin (najprej spoilerjev).\",")
+                .append("\"mikrostoritve\": [\"TBDhttp://35.189.96.118:8081/v1/orders\", \"TBDhttp://35.197.209.159:8080/v1/customers/\"],")
+                .append("\"github\": [\"https://github.com/Content-blocker/content-provider\", \"https://github.com/Content-blocker/ai\"],")
+                .append("\"travis\": [\"https://travis-ci.org/Content-blocker/content-provider\", \"https://travis-ci.org/Content-blocker/ai\"],")
+                .append("\"dockerhub\": [\"https://cloud.docker.com/repository/docker/sr123/provider/\", \"https://cloud.docker.com/repository/docker/sr123/ai/\"]")
+                .toString();
+        return "{" + r + "}";
     }
 }
---
+++ provider-api/src/main/java/com/kumuluzee/blocker/provider/api/RestApplication.java
@@ -0,0 +1,10 @@
+package com.kumuluzee.blocker.provider.api;
+
+import javax.xml.ws.Response;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
