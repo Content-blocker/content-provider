0fd04143999f23a83861774396bf6c0b7a366b67
ssandir <sandi.rezonja@gmail.com>
2020-01-08 23:50:25 +0100

    added k8s for fetcher

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /provider-api
 
 ADD ./provider-api/target/provider-api-1.0.0.jar /provider-api
 
-EXPOSE 8080
+EXPOSE 8081
 
 CMD ["java", "-jar", "provider-api-1.0.0.jar"]
--- provider-api/src/main/java/com/kumuluzee/blocker/provider/api/ProviderApi.java
+++ provider-api/src/main/java/com/kumuluzee/blocker/provider/api/ProviderApi.java
@@ -18,8 +18,10 @@ import java.util.Optional;
 public class ProviderApi {
     static Optional<WebTarget> aiTarget = CustomDiscovery.discover("ai", "test", "1.0.0");
     static Optional<WebTarget> providerTarget = CustomDiscovery.discover("provider", "test", "1.0.0");
+    static Optional<WebTarget> fetcherTarget = CustomDiscovery.discover("fetcher", "test", "1.0.0");
     static String aiString = (aiTarget.isPresent() ? aiTarget.get().getUri().toString() : "Empty");
     static String providerString = (providerTarget.isPresent() ? providerTarget.get().getUri().toString() : "Empty");
+    static String fetcherString = (fetcherTarget.isPresent() ? fetcherTarget.get().getUri().toString() : "Empty");
 
     @GET
     @Timed
@@ -29,12 +31,18 @@ public class ProviderApi {
         String links = "";
         if(providerTarget.isPresent()){
             links += "<a href='"+ providerString + "/provider/api/integrations'>provider/api/integrations</a><br>";
+            links += "<a href='"+ providerString + "/provider/api/integrations'>provider/api/makelog</a><br>";
+            links += "<a href='"+ providerString + "/provider/api/give-ebola'>provider/api/give-ebola</a><br>";
             links += "<a href='"+ providerString + "/provider/discovery'>provider/discovery</a><br>";
         }
-        if(aiTarget.isPresent()){
-            links += "<a href='"+ aiString + "/ai/api'>ai/api/</a><br>";
-        }
-        return "Hellow world! <br> I provide. <br>" + links;
+        return "Hellow world! <br> I provide. <br><br>" + links;
+    }
+
+    @GET
+    @Timed
+    @Path("/give-ebola")
+    public String giveebola() {
+        return "Imune to ebola.";
     }
 
     @GET
@@ -48,17 +56,37 @@ public class ProviderApi {
 
     @GET
     @Timed
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Path("/integrations")
     public String integrations() {
-        String out = "";
+        String out = "<body> <h5> ai integrations </h5>";
+
+        out += "<div> ai: ";
         if (aiTarget.isPresent()) {
-            out += "ai: " + aiString + "\n";
+            out += "ai: " + aiString + "<br>";
+            out += "<a href='"+ aiString + "/ai/api'>ai/api</a><br>";
         }
-        else out+= "missing ai \n";
-        if (providerTarget.isPresent()) {
-            out += "provider: " + providerString + "\n";
+        else out+= "missing <br>";
+        out += "</div>";
+
+        out += "<div> provider: ";
+        if (aiTarget.isPresent()) {
+            out += "provider: " + providerString + "<br>";
+            out += "<a href='"+ providerString + "/provider/api'>provider/api</a><br>";
         }
-        else out+= "missing provider \n";
+        else out+= "missing <br>";
+        out += "</div>";
+
+        out += "<div> fetcher: ";
+        if (aiTarget.isPresent()) {
+            out += "fetcher: " + fetcherString + "<br>";
+            out += "<a href='"+ fetcherString + "/fetcher/api'>fetcher/api</a><br>";
+        }
+        else out+= "missing <br>";
+        out += "</div>";
+
+        out += "</body>";
         return out;
     }
 }
